f8e0fc263a31570c95a4ee873fde7af59c03eeda
SherwoodA22 <amanalek@gmail.com>
2025-05-06 11:58:02 -0400

    Added filtering
    Added the ability to filter by completion, this rotates between completed, not completed, and all.

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -8,7 +8,8 @@ interface TodoListProps {
 
 const  TodoList: React.FC<TodoListProps> = ({initialTodos}) => {
     const [todos, setTodos] = useState<Todo[]>(initialTodos);
-    const [editingTodo, setEditingTodo] = useState<number | null>(null);
+    const [fullTodo, setFullTodo] = useState<Todo[]>(initialTodos);
+    const [filter, setFilter] = useState(0);
 
     function addTodo(t: string) {
         const create = document.getElementById(t) as HTMLInputElement | null;
@@ -16,6 +17,7 @@ const  TodoList: React.FC<TodoListProps> = ({initialTodos}) => {
             if (create.value !== ""){
             const newTodo: Todo = {userId: 1, id: Date.now(), completed: false, title: create.value }
                 setTodos([...todos, newTodo])
+                setFullTodo([...todos, newTodo]);
             } else {
                 alert("Title Should not Be Empty");
             }
@@ -33,6 +35,15 @@ const  TodoList: React.FC<TodoListProps> = ({initialTodos}) => {
         }
             
         ))
+        setFullTodo(prevTodos => prevTodos.map(todo => {
+            if (todo.id === id) {
+                return { ...todo, title: value };
+            } else {
+                return todo;
+            }
+        }
+            
+        ))
         } else {
             alert("Please enter a title.")
         }   
@@ -40,6 +51,7 @@ const  TodoList: React.FC<TodoListProps> = ({initialTodos}) => {
     
     function deleteTodo(id: number) {
         setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+        setFullTodo(prevTodos => prevTodos.filter(todo => todo.id !== id));
     }
 
     function toggleTodo(id: number) {
@@ -47,11 +59,34 @@ const  TodoList: React.FC<TodoListProps> = ({initialTodos}) => {
             if (todo.id === id) {
                 return { ...todo, completed: !todo.completed };
             } else {
-               
                 return todo;
             }
         }
         ));
+        setFullTodo(prevTodos => prevTodos.map(todo => {
+            if (todo.id === id) {
+                return { ...todo, completed: !todo.completed };
+            } else {
+                return todo;
+            }
+        }
+        ));
+    }
+
+    function filterTodo() {
+        if (filter === 0) {
+            setFilter(1)
+            setTodos(fullTodo.filter(todo => !todo.completed))
+  
+        } else if (filter === 1) {
+            setFilter(2)
+            setTodos(fullTodo.filter(todo => todo.completed))
+
+        } else {
+            setFilter(0)
+            setTodos(fullTodo)
+
+        }
     }
     
     return (
@@ -59,6 +94,7 @@ const  TodoList: React.FC<TodoListProps> = ({initialTodos}) => {
             <p id="title">Todo List:</p>
             <input id="create" placeholder="Create New Todo"></input>
             <button onClick={() => addTodo("create")}>Create</button>
+            <button onClick={() => filterTodo()}>Filter By Completed</button>
             {todos.map((todo) => {
         return (
             <div key={todo.id}>
--- src/app/todos/loading.tsx
+++ src/app/todos/loading.tsx
@@ -1,3 +1,3 @@
 export default function Loading() {
-    return <div id="loading">Loading dashboard...</div>;
+    return <div id="loading">Loading TodoList...</div>;
   }
